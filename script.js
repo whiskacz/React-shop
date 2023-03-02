2ab1d64df8c8634064065f58d65dfb92ec18b5ed
whiskacz <pawelblasiak1985@gmail.com>
2023-03-02 22:02:58 +0100

    React shop practice sass

---
+++ script.js
@@ -0,0 +1,14 @@
+class Shop extends React.Component{
+
+    render(){
+        return(
+            <>
+            <button>-</button>
+            <span>elo elo</span>
+            <button>+</button>
+            </>
+        )
+    }
+}
+
+ReactDOM.render(<Shop />, document.getElementById('root'))
