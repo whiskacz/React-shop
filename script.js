b55db0293b1683a864624b4463c60ad33cff0d28
whiskacz <pawelblasiak1985@gmail.com>
2023-03-03 17:45:02 +0100

    React shop with availability state

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 class Shop extends React.Component{
+state = {
+    availability : 9,
+    currentState : 0
+}
+handleRemoveFromCart = () => {
+    this.setState({
+        currentState: this.state.currentState - 1
+    })
+}
+handleAddFromCart = () => {
+    this.setState({
+        currentState: this.state.currentState + 1
+    })
+}
+buyButton = () => {
+    this.setState({
+        availability : this.state.availability - this.currentState,
+        currentState : 0
+        
+    })
+}
+
 
     render(){
         return(
             <>
-            <button>-</button>
-            <span>elo elo</span>
-            <button>+</button>
+            <button disabled={this.state.currentState === 0 ? true : false} onClick={this.handleRemoveFromCart}>-</button>
+            <span style={{
+                backgroundColor : "red",
+                color: "blue"
+            }}>{this.state.currentState}</span>
+            <button disabled={this.state.currentState >= this.state.availability? true : false}onClick={this.handleAddFromCart}>+</button>
+            <button onClick={this.buyButton}>KUP</button>
             </>
         )
+       
     }
 }
 
